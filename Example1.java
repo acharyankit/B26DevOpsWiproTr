373e4ab5e7fb5b6e4c89cbf9bacb26cd7128ce61
acharyankit <ankit.pracharya@gmail.com>
2020-09-19 20:00:20 +0530

    trainingFirstV2

--- Example1.java
+++ Example1.java
@@ -4,5 +4,6 @@ public static void main (String[] args){
 
 System.out.println("Hello... Let's start with DevOps!!");
 System.out.println("Changes are done... V1");
+System.out.println("Changes are done... V2");
 }
 }

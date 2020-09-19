9ee12f640ec408d13ca48c2ccb31c5338e2a942f
acharyankit <ankit.pracharya@gmail.com>
2020-09-19 21:21:43 +0530

    trainingFirstV3

--- Example1.java
+++ Example1.java
@@ -5,5 +5,6 @@ public static void main (String[] args){
 System.out.println("Hello... Let's start with DevOps!!");
 System.out.println("Changes are done... V1");
 System.out.println("Changes are done... V2");
+System.out.println("Changes done in Fetched from remote... V3");
 }
 }
